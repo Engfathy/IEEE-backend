efc46a6cce4a5dcadd2d8509dc6f173ed7382046
Engfathy <fathy01015485028@gmail.com>
2023-03-02 21:43:10 +0200

    add php dir

---
+++ php/task-1/problem1.php
@@ -0,0 +1,20 @@
+<?php
+/*
+any thing
+hello
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset=<?='"UTF-8"'?>>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?='My First PHP Page'?></title>
+  </head>
+  <body>
+    <div><?php echo 'We love'; ?></div>
+    <div><?php echo 'Elzero channel'; ?></div>
+  </body>
+</html>
---
+++ php/task-1/problem7.php
@@ -0,0 +1,5 @@
+<?php
+echo "hello \"Elzero\" \\\\ \"\"\" We Love \"$\$PHP\"";
+
+// Needed Output
+// Hello "Elzero" \\ """ We Love "$$PHP"
