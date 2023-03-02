20d2554a5eb270733fb958939b2200138d51cdc1
Engfathy <fathy01015485028@gmail.com>
2023-03-02 21:38:46 +0200

    remove php

--- php/task-1/problem4.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// ## First Comment   => good
-# // # Second Comment  => good
-/* /* /* Third Comment */  // => good
-////// Fourth Comment   => good
--- php/task-1/problem5.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo 15.2 + 14.7 + (10.5 + 10.5); // 50
-echo 15.2 + 14.7 + (10.5 + 10.5); // Integer
-// solution  =>
-echo (int) (15.2 + 14.7) + (int)(10.5 + 10.5); // 50
-echo gettype((int) (15.2 + 14.7) + (int)(10.5 + 10.5)); // Integer
